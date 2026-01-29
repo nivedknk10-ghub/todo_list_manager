6a2f392f4f42ea17b6d95afb56e405137b7cda4f
devin <devin1123cyber@gmail.com>
2026-01-29 19:40:57 +0530

    Add task class and basic usage

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 #this it the main file
+from task import Task
+
 def main():
-    print("Welcome to the to-do list manager")
+    task1 = Task("Learn github workflow")
+    print(task1.title)
+    print(task1.completed)
+
+    task1.mark_completed()
+    print(task1.completed)
 
 if __name__ == "__main__":
     main()
