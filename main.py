2be985cebf9d64e705406abea96211930b38ccdc
devin <devin1123cyber@gmail.com>
2026-01-30 19:32:58 +0530

    Add interactive command-line menu

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-#this it the main file
 from manager import Taskmanager
+
+def show_menu():
+    print("\nTo-Do List Manager\n")
+    print("1. Add Task\n")
+    print("2. View Task\n")
+    print("3. Complete Task\n")
+    print("4. Exit\n")
 
 def main():
     manager = Taskmanager()
 
-    manager.add_task("Learn OSINT")
-    manager.add_task("Buy Galaxy A36 5G")
+    while True:
+        show_menu()
+        choice = input("Enter your choice(1-4): ")
 
-    manager.list_tasks()
-    manager.complete_task(1)
-    print("\nAfter completing the first task:\n")
-    manager.list_tasks()
+        if choice == "1":
+            title = input("Enter Task Title: ")
+            manager.add_task(title)
+            print("Task Added")
+
+        elif choice == "2":
+            manager.list_tasks()
+
+        elif choice == "3":
+            number = int(input("Enter Task Number: "))
+            manager.complete_task(number)
+            print("Task Completed")
+
+        elif choice == "4":
+            print("Exiting...")
+            break
+
+        else:
+            print("Invalid Choice")
+
 
 if __name__ == "__main__":
     main()
